fd598d9db60db683ebb7484df76f5fb52c113569
Sujal Vaishnav <sujalvaishnav30804@gmail.com>
2026-02-11 22:42:57 +0530

    Phase 10 Completed

--- Django_CS/django_project/blog/views.py
+++ Django_CS/django_project/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin, #We will use mixins as .login_required can't be used in class based views!!
+    UserPassesTestMixin,
+)
 
 # Create your views here.
 
@@ -11,17 +15,46 @@ def home(request):
 
     return render(request, 'blog/home.html', context)
 
-class PostListView(ListView):
+class PostListView(ListView): #Using class based views instead of fnx based views.
     model = Post
-    template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
+    template_name = 'blog/home.html' #Convention-1 : <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
 class PostDetailView(DetailView):
-    model = Post
+    model = Post #Create a new html page using Convention-1 which it will then use the template directly
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
+    fields = ['title','content']
+
+    def form_valid(self,form): # A fnx to let the user(which is currently signed in) be the author of the post.
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self,form): # A fnx to let the user(which is currently signed in) be the author of the post.
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self): # To prevent any user to update other users post!!
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post 
+    success_url = '/'
+    def test_func(self): # To prevent any user to update other users post!!
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 def about(request):
     return render(request,'blog/about.html', {'title':'About'})
