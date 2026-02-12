54940e4ecb6ba4bea7c4db620ed4988ad5911a64
Sujal Vaishnav <sujalvaishnav30804@gmail.com>
2026-02-12 19:04:07 +0530

    Phase 9 completed

--- Django_CS/django_project/blog/views.py
+++ Django_CS/django_project/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.models import User
 from .models import Post
 from django.contrib.auth.mixins import (
     LoginRequiredMixin, #We will use mixins as .login_required can't be used in class based views!!
@@ -20,6 +21,18 @@ class PostListView(ListView): #Using class based views instead of fnx based view
     template_name = 'blog/home.html' #Convention-1 : <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
+
+class UserPostListView(ListView): #Using class based views instead of fnx based views.
+    model = Post
+    template_name = 'blog/user_posts.html' #Convention-1 : <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post #Create a new html page using Convention-1 which it will then use the template directly
