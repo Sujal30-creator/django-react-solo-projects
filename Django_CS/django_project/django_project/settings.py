fe7758d6265ccdb5988002d734aeefb27a2a5d04
Sujal Vaishnav <sujalvaishnav30804@gmail.com>
2026-02-12 23:52:26 +0530

    Phase 11,12 completed

--- Django_CS/django_project/django_project/settings.py
+++ Django_CS/django_project/django_project/settings.py
@@ -134,3 +134,19 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+# django_project/settings.py
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# Replace with your actual Gmail address
+EMAIL_HOST_USER = 'sujalvaishnav30804@gmail.com'
+
+# Replace with the 16-character App Password you generated in Step 1
+EMAIL_HOST_PASSWORD = 'nsvb ehfg mmck hjmg'
+
+# This fixes the 'webmaster@localhost' error
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
